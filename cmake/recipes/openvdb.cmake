bf8699ee7576668d3a9c34289399ca5c4c9c6776
ZizhouHuang <zizhou@nyu.edu>
2025-05-28 17:17:51 -0400

    Update openvdb

--- cmake/recipes/openvdb.cmake
+++ cmake/recipes/openvdb.cmake
@@ -7,8 +7,9 @@ endif()
 
 message(STATUS "Third-party: creating target 'openvdb::openvdb'")
 
+option(OPENVDB_BUILD_PYTHON_MODULE "Build the pyopenvdb Python module" OFF)
 option(OPENVDB_CORE_SHARED "Build dynamically linked version of the core library." OFF)
 option(OPENVDB_ENABLE_UNINSTALL "Adds a CMake uninstall target." OFF)
 
 include(CPM)
-CPMAddPackage("gh:Huangzizhou/openvdb#f9e84246f17b0870451dfd27f97bc8d1a92ec8f4")
+CPMAddPackage("gh:Huangzizhou/openvdb#c96cb06971a89ad2638ed29972ab54f63a8e2fbc")
