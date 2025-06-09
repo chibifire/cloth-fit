926064bdb3143936a4e8234b4e121da67fc0d6e3
ZizhouHuang <zizhou@nyu.edu>
2025-06-08 23:13:22 -0400

    disable some linear solvers

--- cmake/recipes/polysolve.cmake
+++ cmake/recipes/polysolve.cmake
@@ -7,5 +7,14 @@ endif()
 
 message(STATUS "Third-party: creating target 'polysolve'")
 
+option(POLYSOLVE_WITH_CHOLMOD       "Enable Cholmod library"                             ON)
+option(POLYSOLVE_WITH_UMFPACK       "Enable UmfPack library"                            OFF)
+option(POLYSOLVE_WITH_SUPERLU       "Enable SuperLU library"                            OFF)
+option(POLYSOLVE_WITH_CUSOLVER      "Enable cuSOLVER library"                           OFF)
+option(POLYSOLVE_WITH_PARDISO       "Enable Pardiso library"                            OFF)
+option(POLYSOLVE_WITH_HYPRE         "Enable hypre"                                      OFF)
+option(POLYSOLVE_WITH_AMGCL         "Use AMGCL"                                         OFF)
+option(POLYSOLVE_WITH_SPECTRA       "Enable Spectra library"                            OFF)
+
 include(CPM)
 CPMAddPackage("gh:Huangzizhou/polysolve#ee553dc6a330ef70bcc6ba2f05653fecd201c05f")
